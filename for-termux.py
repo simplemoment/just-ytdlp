fb1ec882a8f3269208b213b59ca0b6734be0f2ac
simplemoment <109022902+simplemoment@users.noreply.github.com>
2024-11-14 16:13:19 +0700

    new

---
+++ for-termux.py
@@ -0,0 +1,16 @@
+from yt_dlp import YoutubeDL
+
+def run():
+    try:
+        ydl_opts = {
+            'format': 'best',
+            'outtmpl': 'downloads/%(title)s.%(ext)s',
+            }
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([self.url])
+    except Exception as e:
+        print(f"{e}")
+
+if __name__ == '__main__':
+    print("enter video url, like a 'https://youtu.be/qwerty01234' or 'https://youtube.com/watch?v=abcdf01234'")
+    input("•> ")
